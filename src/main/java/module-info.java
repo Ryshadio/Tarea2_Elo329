89755830771bdcfa91d07b894ed05ba86feaa732
Ryshadio <104236602+Ryshadio@users.noreply.github.com>
2022-06-11 23:38:36 -0400

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.tarea.tarea2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.media;
-
-
-    opens com.tarea.tarea2 to javafx.fxml;
-    exports com.tarea.tarea2;
-}
